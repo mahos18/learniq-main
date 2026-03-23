916f0d56adf4da2f944780043ffc915901761f27
mahos18 <sohamlohote@gmail.com>
2026-03-23 19:59:01 +0530

    quiz fixed

--- src/components/module/ContentBlock.tsx
+++ src/components/module/ContentBlock.tsx
@@ -3,7 +3,7 @@
 import ReactMarkdown from "react-markdown";
 import { getYouTubeId } from "@/lib/utils";
 import type { ContentBlockClient } from "@/types";
-import QuizBlock from "./QuizBlock";
+import QuizBlock from "../quiz/QuizBlock";
 import Image from "next/image";
 
 interface ContentBlockProps {
